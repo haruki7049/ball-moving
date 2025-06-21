ebfec37c88ebc074d339ff5ced161b5110bdef00
haruki7049 <tontonkirikiri@gmail.com>
2025-06-21 16:44:41 +0900

    Update

--- src/main.lua
+++ src/main.lua
@@ -1,27 +1,54 @@
 function love.load()
-  ball = { x = 5; y = 5; radiusx = 25; radiusy = 25; color = { r = 1, g = 0, b = 0, }, }
+  red_ball = { x = 5; y = 5; radiusx = 25; radiusy = 25; color = { r = 1, g = 0, b = 0, }, }
+  blue_ball = { x = 5; y = 5; radiusx = 25; radiusy = 25; color = { r = 0, g = 0, b = 1, }, }
 end
 
 function love.update()
+  -- Exit by escape key
+  if love.keyboard.isDown("escape") then
+    love.event.quit(0)
+  end
+
+  -- Red ball
   if love.keyboard.isDown("left") then
-    ball.x = ball.x - 5
+    red_ball.x = red_ball.x - 5
   end
   if love.keyboard.isDown("right") then
-    ball.x = ball.x + 5
+    red_ball.x = red_ball.x + 5
   end
   if love.keyboard.isDown("up") then
-    ball.y = ball.y - 5
+    red_ball.y = red_ball.y - 5
   end
   if love.keyboard.isDown("down") then
-    ball.y = ball.y + 5
+    red_ball.y = red_ball.y + 5
+  end
+
+  -- Red ball
+  if love.keyboard.isDown("a") then
+    blue_ball.x = blue_ball.x - 5
+  end
+  if love.keyboard.isDown("d") then
+    blue_ball.x = blue_ball.x + 5
+  end
+  if love.keyboard.isDown("w") then
+    blue_ball.y = blue_ball.y - 5
+  end
+  if love.keyboard.isDown("s") then
+    blue_ball.y = blue_ball.y + 5
   end
 end
 
 function love.draw()
-  drawBall()
+  drawRedBall()
+  drawBlueBall()
 end
 
-function drawBall()
-    love.graphics.setColor(ball.color.r, ball.color.g, ball.color.b)
-    love.graphics.ellipse("fill", ball.x, ball.y, ball.radiusx, ball.radiusy, 100)
+function drawRedBall()
+    love.graphics.setColor(red_ball.color.r, red_ball.color.g, red_ball.color.b)
+    love.graphics.ellipse("fill", red_ball.x, red_ball.y, red_ball.radiusx, red_ball.radiusy, 100)
+end
+
+function drawBlueBall()
+    love.graphics.setColor(blue_ball.color.r, blue_ball.color.g, blue_ball.color.b)
+    love.graphics.ellipse("fill", blue_ball.x, blue_ball.y, blue_ball.radiusx, blue_ball.radiusy, 100)
 end
